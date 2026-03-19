7977adc9e69fa5cef784a3313e95a8d693b5d34a
Jaswanthroyal <pallajaswanthroyal@gmail.com>
2026-03-19 17:30:13 +0530

    sleep test files

---
+++ code/test/sleep_test.c
@@ -0,0 +1,6 @@
+#include "syscall.h"
+
+int main(){
+    Sleep(10);
+    return 0;
+}
---
+++ code/test/sleep_test_simple.c
@@ -0,0 +1,8 @@
+// sleep_test_simple.c
+#include "syscall.h"
+
+int main() {
+    Sleep(5);
+    Sleep(20);
+    return 0;
+}   
